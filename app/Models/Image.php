121dce2864da2cd804410d81360a714bafff6889
Skidis17 <moha-sadki@outlook.com>
2025-05-05 14:31:06 +0100

    ii

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
-
+{
     use HasFactory;
 
     protected $fillable = ['url', 'objet_id'];
