8ac1d32e3a4d4d5c1ac574c584591b48a40b26ec
Mohamed Bensaddik <134942556+MedBens02@users.noreply.github.com>
2025-05-15 13:09:19 +0100

    Add files via upload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'role' => \App\Http\Middleware\CheckRole::class,
+            'checkUserStatus' => \App\Http\Middleware\CheckUserStatus::class, 
+            'guestany' => \App\Http\Middleware\RedirectIfAuthenticatedAnyGuard::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
