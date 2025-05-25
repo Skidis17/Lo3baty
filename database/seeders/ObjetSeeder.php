21bafd5dbdd5d9396371687d001386c47176028a
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2025-05-25 15:47:30 -0500

    fix

---
+++ database/seeders/ObjetSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Objet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ObjetSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Objet::insert([
+            [
+                'nom' => 'Lego Star Wars',
+                'description' => 'Un set complet de Lego Star Wars.',
+                'ville' => 'Tétouan',
+                'etat' => 'Neuf',
+                'categorie_id' => 3,
+                'proprietaire_id' => 1,
+            ],
+            [
+                'nom' => 'Voiture télécommandée',
+                'description' => 'Voiture rapide et résistante.',
+                'ville' => 'Tétouan',
+                'etat' => 'Bon état',
+                'categorie_id' => 5,
+                'proprietaire_id' => 1,
+                
+            ],
+            [
+                'nom' => 'Voiture rouge',
+                'description' => 'Voiture rapide.',
+                'ville' => 'Tétouan',
+                'etat' => 'Usage',
+                'categorie_id' => 3,
+                'proprietaire_id' => 1,
+            ],
+        ]);
+    }
+}
+
